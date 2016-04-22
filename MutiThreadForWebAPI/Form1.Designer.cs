7a78e26dac900c4b86526dffa88bab9134d10d1a
unknown <NSW00_905528@nsw.local>
2016-04-22 18:13:08 +0900

    Commit sample for testing mutiThread to check postalCode WebAPI

---
+++ MutiThreadForWebAPI/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace MutiThreadForWebAPI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(152, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(311, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "WebAPI請求実行";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "スレッド数：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(194, 117);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 19);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "郵便番号ファイル：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(93, 213);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 16);
+            this.label4.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(93, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 12);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "請求回数：";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(194, 147);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 19);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(312, 124);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 12);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "※デフォルト：4";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(312, 154);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(85, 12);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "※デフォルト：200";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(63, 177);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(89, 12);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "総API送信回数：";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(164, 253);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(233, 12);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "WebAPI戻り値チェックログ：　　./Log/nlogfile.log";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(192, 177);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(135, 12);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "郵便番号数() * 請求回数";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(194, 35);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(259, 19);
+            this.textBox3.TabIndex = 13;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(478, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(73, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "ファイル選択";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 12);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "WebAPIエンドポイント：";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(194, 62);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(259, 19);
+            this.textBox4.TabIndex = 16;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(207, 84);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(204, 12);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "※例：http://yubin.senmon.net/service/";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(52, 290);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(98, 12);
+            this.label12.TabIndex = 18;
+            this.label12.Text = "WebAPI請求説明：";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(93, 319);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(272, 12);
+            this.label13.TabIndex = 19;
+            this.label13.Text = "①：同一郵便番号に対して、請求回数分の請求をする。";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(93, 345);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(363, 12);
+            this.label14.TabIndex = 20;
+            this.label14.Text = "②：請求回数ループの中に、奇数請求回数の場合、下記のように請求する。";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(93, 388);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(363, 12);
+            this.label15.TabIndex = 21;
+            this.label15.Text = "③：請求回数ループの中に、偶数請求回数の場合、下記のように請求する。";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(107, 366);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(158, 12);
+            this.label16.TabIndex = 22;
+            this.label16.Text = "エンドポイントexists/?郵便番号";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(107, 411);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(164, 12);
+            this.label17.TabIndex = 23;
+            this.label17.Text = "エンドポイントexists/?郵便番号-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 446);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "複数スレッドWebAPIテスト";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+    }
+}
+
---
+++ MutiThreadForWebAPI/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NLog;
+
+namespace MutiThreadForWebAPI
+{
+    public partial class Form1 : Form
+    {
+        #region 定数
+
+        //デフォルトスレッド数
+        private static int defaultThreadCount = 4;
+
+        //デフォルト請求回数
+        private static int defaultRequestCount = 200;
+
+        //ロガー
+        private static Logger logger = LogManager.GetCurrentClassLogger(); 
+
+        #endregion
+
+        #region 変数
+
+        //WebAPIエンドポイント
+        string endPoint = "";
+
+        //スレッド数
+        int threadCount = 0;
+
+        //請求回数
+        int requestCount = 0;
+
+        //郵便番号
+        List<string> postalCodesList = new List<string>();
+        #endregion
+
+        #region インスタンス
+        /// <summary>
+        /// インスタンス
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region イベント
+        /// <summary>
+        /// WebAPI請求実行ボタン押下イベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //入力有効性チェック
+            if(CheckInputValid())
+            {
+                button1.Hide();
+                label4.Text = @"WebAPI請求処理中.......終了のメッセージ出すまで待ってください。";
+
+                try
+                {
+                    var po = new ParallelOptions();
+
+                    po.MaxDegreeOfParallelism = threadCount;
+
+                    string[] postalCodes = postalCodesList.ToArray();
+                    Parallel.ForEach(postalCodes, po, postalCode => { SendWebAPIRequest(postalCode); });
+
+                }
+                catch
+                {
+                    MessageBox.Show("異常発生しました。");
+                }
+                finally
+                {
+                    button1.Show();
+                    label4.Text = "";
+                    MessageBox.Show("処理終了。");
+                }
+            }            
+        }
+        /// <summary>
+        /// ファイル選択ボタン押下イベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = @"C:\";
+            openFileDialog1.Title = "ファイルを選択してください";
+
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.CheckPathExists = true;
+
+            openFileDialog1.DefaultExt = "csv";
+            openFileDialog1.Filter = "TXTファイル|*.txt|CSVファイル|*.csv|すべてのファイル|*.*";
+            openFileDialog1.FilterIndex = 3;
+            openFileDialog1.RestoreDirectory = true;
+
+            openFileDialog1.ReadOnlyChecked = true;
+            openFileDialog1.ShowReadOnly = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.textBox3.Text = openFileDialog1.FileName;
+            }
+        }
+        #endregion
+
+        #region 内部メソッド
+        /// <summary>
+        /// WebAPI請求
+        /// </summary>
+        /// <param name="postalCode"></param>
+        private void SendWebAPIRequest(string postalCode)
+        {
+            string strUrl = "";
+            for (int i = 1; i <= requestCount; i++)
+            {
+                strUrl = endPoint;
+                if (i % 2 == 1)
+                {
+                    strUrl = String.Format("{0}exists/?{1}", endPoint, postalCode);
+                }
+                else if (i % 2 == 0)
+                {
+                    strUrl = String.Format("{0}exists/?{1}-", endPoint, postalCode);
+                }
+                logger.Info(strUrl + Environment.NewLine);
+                WebRequest webreq = WebRequest.Create(strUrl);
+                //サーバーからの応答を受信するためのHttpWebResponseを取得
+                var webres = (System.Net.HttpWebResponse)webreq.GetResponse();
+                //応答ステータスコードを判定 
+                if (webres.StatusCode == HttpStatusCode.OK)
+                {
+                    var stream = webres.GetResponseStream();
+                    var reader = new StreamReader(stream, Encoding.GetEncoding("Shift_JIS"));
+                    var responseString = reader.ReadToEnd();
+
+                    logger.Info(responseString + Environment.NewLine);
+                }
+            }
+            return;
+        }
+        /// <summary>
+        /// 入力有効性チェック
+        /// </summary>
+        /// <returns></returns>
+        private bool CheckInputValid()
+        {
+            //WebAPIエンドポイント入力チェック
+            if (string.IsNullOrEmpty(this.textBox4.Text))
+            {
+                MessageBox.Show("WebAPIエンドポイントは入力不正です！");
+                return false;
+            }
+            else
+            {
+                endPoint = this.textBox4.Text;
+            }
+
+            //WebAPIエンドポイント入力チェック
+            if (Int32.TryParse(this.textBox2.Text, out requestCount) == false)
+            {
+            }
+
+            //スレッド数入力チェック
+            if (Int32.TryParse(this.textBox1.Text, out threadCount) == false)
+            {
+                DialogResult result = MessageBox.Show(string.Format("スレッド数異常入力：デフォルトスレッド数：{0} で実行しますか？", defaultThreadCount), 
+                    "質問",
+                    MessageBoxButtons.OKCancel,
+                    MessageBoxIcon.Exclamation,
+                    MessageBoxDefaultButton.Button2);
+                if (result == DialogResult.OK)
+                {
+                    threadCount = defaultThreadCount;
+                }
+                else if (result == DialogResult.Cancel)
+                {
+                    return false;
+                }
+            }
+            //請求回数入力チェック
+            if (Int32.TryParse(this.textBox2.Text, out requestCount) == false)
+            {
+                DialogResult result = MessageBox.Show(string.Format("請求回数異常入力：デフォルト請求回数：{0} で実行しますか？", defaultRequestCount),
+                    "質問",
+                    MessageBoxButtons.OKCancel,
+                    MessageBoxIcon.Exclamation,
+                    MessageBoxDefaultButton.Button2);
+                if (result == DialogResult.OK)
+                {
+                    requestCount = defaultRequestCount;
+                }
+                else if (result == DialogResult.Cancel)
+                {
+                    return false;
+                }
+            }
+            //郵便番号選択入力チェック
+            if (File.Exists(this.textBox3.Text))
+            {
+                postalCodesList.Clear();
+                try
+                {
+                    string value;
+                    using (TextReader fileReader = File.OpenText(this.textBox3.Text))
+                    using (var csv = new CsvHelper.CsvReader(fileReader))
+                    {
+                        csv.Configuration.HasHeaderRecord = false;
+                        while (csv.Read())
+                        {
+                            for (int i = 0; csv.TryGetField<string>(i, out value); i++)
+                            {
+                                postalCodesList.Add(value);
+                            }
+                        }
+                        if (postalCodesList.Count <= 0)
+                        {
+                            MessageBox.Show("郵便番号ファイルに何もないです！");
+                            return false;
+                        }
+                        else
+                        {
+                            this.label10.Text = string.Format("郵便番号数({0}) * 請求回数 : {1}", postalCodesList.Count, postalCodesList.Count * requestCount);
+                        }
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("郵便番号ファイルを正常に読み込めませんでした！");
+                    return false;
+                }
+            }
+            else
+            {
+                // filePathのファイルは存在しない
+                MessageBox.Show("郵便番号ファイルを選択してください！");
+                return false;
+            }
+            return true;
+        }
+        #endregion
+    }
+
+    ///// <summary>
+    ///// 格納用クラス
+    ///// </summary>
+    //public class CarInfo
+    //{
+    //    public string carCode { get; set; }
+    //    public string updYmd { get; set; }
+    //}
+
+    ///// <summary>
+    ///// マッピング用クラス
+    ///// </summary>
+    //public sealed class CarInfoMapper : CsvHelper.Configuration.CsvClassMap<CarInfo>
+    //{
+    //    public CarInfoMapper()
+    //    {
+    //        Map(x => x.carCode).Index(0);
+    //        Map(x => x.updYmd).Index(1);
+    //    }
+    //}
+}
